e2a35b48f7d778a6b1b8f87dd1da7553f9373b5e
박종범 <jbpark03@gmail.com>
2025-05-07 14:53:03 +0900

    onClick callback function specified

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,20 @@ function App() {
   return (
     <>
       <h1>계수기</h1>
-<button className="button ms-3" onClick={addCounter}>계수기 추가</button>
-<ul>
-  {counters.map(counter => {
-    // counter <id> : <value> <button>
-    return (
-      <li key={counter.id}>
-        counter {counter.id} : {counter.value}
-        <button className='button ms-3' onClick={() => { }}>증가</button>
-      </li>
-    );
-  })}
-</ul>
+      <button className="button ms-3" onClick={addCounter}>계수기 추가</button>
+      <ul>
+        {counters.map(counter => {
+          // counter <id> : <value> <button>
+          return (
+            <li key={counter.id}>
+              counter {counter.id} : {counter.value}
+              <button className='button ms-3' 
+                      onClick={() => {incCount(counter.id) }}>
+                증가</button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   )
 }
