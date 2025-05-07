093e30f8fc26e871f309a8534204d9a9aad70ce8
박종범 <jbpark03@gmail.com>
2025-05-07 18:37:46 +0900

    checkbox input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const handleChange = (e) => {
-}
-
 function App() {
   const [formData, setFormData] = useState({
     text: "",
@@ -11,19 +8,34 @@ function App() {
     radio: "",
     select: ""
   })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, text: e.target.value });
+  }
+
   return (
     <div className='form-container'>
       <h1>폼 자료</h1>
       <form>
         {/* 텍스트 입력 */}
         <div className='form-field'>
-          <label>텍스트:</label>
-          <input type="text" name="text"
-            value={formData.text}
-            onChange={handleChange}>
-          </input>
+          <label>텍스트 : 
+            <input type="text" name="text"
+              value={formData.text}
+              onChange={handleChange}>
+            </input>
+          </label>
         </div>
         {/* 첵크 상자 */}
+        <div className='form-field'>
+          <label>
+            <input type="checkbox" name="checkbox"
+              checked={formData.checkbox}
+              onChange={handleChange}>
+            </input>
+            : 첵크상자
+          </label>
+        </div>
       </form>
     </div>
   )
